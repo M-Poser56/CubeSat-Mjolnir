5060dc377a4ece1a3835b34befeb28a27e76abb2
Matt Poser <matthew.poser@opg.com>
2026-09-12 11:41:09 -0400

    not so drunken idiocy (added reboot script, made passwordless)

--- FlightComputer/Mjolnir_Reboot.py
+++ FlightComputer/Mjolnir_Reboot.py
@@ -6,8 +6,9 @@
 import queue
 import paho.mqtt.client as mqtt
 import re
+import subprocess
 
-command_queue = queue.Queue() #initialize script local queue to store upstream command messages
+reboot_queue = queue.Queue() #initialize script local queue to store upstream command messages
 
 
 #mqtt functions (for reboot script)
@@ -16,7 +17,7 @@ def on_connect(client, userdata, flags, reason_code, properties):
     client.subscribe("CMD/REBOOT") #upstream being published to by LoRA_ULDL in the CommandHandler() function
 
 def on_message(client, userdata, msg):
-    command_queue.put(msg.payload.decode()) #adds command string to queue, FIFO
+    reboot_queue.put(msg.payload.decode()) #adds command string to queue, FIFO
 
 Reboot_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 Reboot_client.on_connect = on_connect
@@ -24,6 +25,20 @@ Reboot_client.on_message = on_message
 Reboot_client.connect("localhost", 1883)
 Reboot_client.loop_start()
 
+#this system service script is going to be oneshot, as we can only reboot
+#once within an operating system cycle. queue.get() is a blocking function,
+#so once we recieve this command we can just immediately run our
+#system reboot, and have it be done with
 
+recieved_command = reboot_queue.get()
 
+if recieved_command == "CMD:REBOOT": #redundant check, only thing being published to this topic is this command from the handler
+    try:
+        subprocess.run(["sudo", "/usr/sbin/reboot"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"command failed with exit code {e.returncode}")
 
+#I had to make this a passwordless command by adding some configs to
+# sudo visudo -f /etc/sudoers.d/myuser-systemctl, but change for mjolnir-reboot
+#within this file, I added: mjolnir ALL=(root) NOPASSWD: /usr/sbin/reboot
+#now, callable from subprocess or the CLI, sudo /usr/sbin/reboot does not require a password
